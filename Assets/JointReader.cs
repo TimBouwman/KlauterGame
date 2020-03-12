f3d1c98c3c5ac82145a5d02f230507306827c1ed
LiableDuke <43440836+LiableDuke@users.noreply.github.com>
2020-03-12 11:43:59 +0100

    started working in reading the joint rotation

---
+++ Assets/JointReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointReader : MonoBehaviour
+{
+    [System.Serializable]
+    public class Joints
+    {
+        public ConfigurableJoint leftArm0, leftArm1, rightArm0, rightArm1, leftLeg0, leftLeg1, rightLeg0, rightLeg1;
+    }
+
+    [SerializeField] private Joints joints;
+    private Quaternion LA0, LA1, RA0, RA1, LL0, LL1, RL0, RL1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Quaternion getJointRotation(ConfigurableJoint joint)
+    {
+        return (Quaternion.FromToRotation(joint.axis, joint.connectedBody.transform.rotation.eulerAngles));
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [SerializeField] private ConfigurableJoint joint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(getJointRotation(joint));
+    }
+
+    public Quaternion getJointRotation(ConfigurableJoint joint)
+    {
+        return (Quaternion.FromToRotation(joint.axis, joint.connectedBody.transform.rotation.eulerAngles));
+    }
+}
