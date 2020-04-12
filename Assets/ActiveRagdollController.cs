f7328b6db112182cacac6e75f6e0b18caaa3eef2
Tim Bouwman <43440836+TimBouwman@users.noreply.github.com>
2020-04-12 09:39:39 +0200

    converted bodytracking to physics

---
+++ Assets/ActiveRagdollController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveRagdollController : MonoBehaviour
+{
+    [SerializeField] private AppendagePositionReader positionReader;
+    [SerializeField] private Transform[] appendageIndexes;
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < appendageIndexes.Length; i++)
+        {
+            appendageIndexes[i].localPosition = positionReader.AppendagePositions[i];
+        }
+    }
+}
